79a1868ade14620c722e484b57ca596e0446d4f0
xchalle <xchalle@42.student.fr>
2022-09-26 12:22:14 +0200

    work in progress

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,5 @@
 #include "include/include.hpp"
-
+#include <sys/time.h>
 
 int main(int ac, char** av)
 {
@@ -9,6 +9,10 @@ int main(int ac, char** av)
 		return 0;
 	}
 	std::string tmp = av[1];
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	long long int time1;
+	time1 = (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 	if (tmp == std::string("vector"))
 		test_vector();
 	if (tmp == std::string("map"))
@@ -17,4 +21,15 @@ int main(int ac, char** av)
 		test_set();
 	if (tmp == std::string("stack"))
 		test_stack();
+	if (tmp == std::string("all"))
+	{
+		test_stack();
+		test_set();
+		test_map();
+		test_vector();
+	}
+	gettimeofday(&tv, NULL);
+	long long int time2;
+	time2 = (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	std::cout << "execution time : " << (time2 - time1) << "ms" << std::endl;
 }
